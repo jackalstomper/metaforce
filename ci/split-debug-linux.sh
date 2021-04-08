4703051cc90ca0bb0de04a7d105f965ac6dd9534
Luke Street <luke.street@encounterpc.com>
2021-04-08 12:30:52 -0400

    CI: Test build with ccache

--- ci/split-debug-linux.sh
+++ ci/split-debug-linux.sh
@@ -4,4 +4,4 @@ for f in hecl metaforce-gui metaforce visigen; do
   objcopy --only-keep-debug $f $f.dbg
   objcopy --strip-debug --add-gnu-debuglink=$f.dbg $f
 done
-tar acfv "$GITHUB_WORKSPACE"/debug.tar.xz -- *.dbg
+XZ_OPT=-3 tar acfv "$GITHUB_WORKSPACE"/debug.tar.xz -- *.dbg
--- ci/upload-debug-macos.sh
+++ ci/upload-debug-macos.sh
@@ -5,5 +5,5 @@ for f in hecl metaforce-gui metaforce visigen; do
   strip -S $f
 done
 sentry-cli upload-dif --org axiodl --project metaforce {hecl,metaforce-gui,metaforce,visigen}{,.dSYM} --include-sources
-tar acfv "$GITHUB_WORKSPACE"/debug.tar.xz -- *.dSYM
+tar acfv "$GITHUB_WORKSPACE"/debug.tar.xz --options compression-level=3 -- *.dSYM
 rm -r -- *.dSYM
